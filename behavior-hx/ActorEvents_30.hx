02879f83a686cccf6f8e14746a30302f468fab2b
ryuuahxxp <44300405+ryuuahxxp@users.noreply.github.com>
2019-02-07 10:47:39 -0800

    7/2

---
+++ behavior-hx/ActorEvents_30.hx
@@ -0,0 +1,146 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.Script.*;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+import com.stencyl.models.Joystick;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import openfl.ui.Mouse;
+import openfl.display.Graphics;
+import openfl.display.BlendMode;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.events.TouchEvent;
+import openfl.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class ActorEvents_30 extends ActorScript
+{
+	
+	
+	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	{
+		super(actor);
+		
+	}
+	
+	override public function init()
+	{
+		
+		/* ======================== When Creating ========================= */
+		/* this  prevents the hero from freezing if he Exits the screen  */
+		actor.makeAlwaysSimulate();
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				if(isKeyDown("right"))
+				{
+					actor.setXVelocity(10);
+				}
+				else if(isKeyDown("left"))
+				{
+					actor.setXVelocity(-10);
+				}
+				else
+				{
+					actor.setXVelocity(0);
+				}
+				if(isKeyDown("up"))
+				{
+					actor.setYVelocity(10);
+				}
+				else if(isKeyDown("down"))
+				{
+					actor.setXVelocity(-10);
+				}
+				else
+				{
+					
+				}
+			}
+		});
+		
+		/* ======================== Actor of Type ========================= */
+		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && sameAsAny(getActorType(32), event.otherActor.getType(),event.otherActor.getGroup()))
+			{
+				actor.shout("_customEvent_" + "Death");
+			}
+		});
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				Engine.engine.setGameAttribute("ice cream x", actor.getXVelocity());
+				Engine.engine.setGameAttribute("ice cream y", actor.getYVelocity());
+			}
+		});
+		
+	}
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
